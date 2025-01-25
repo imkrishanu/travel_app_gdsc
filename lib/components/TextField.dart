1d41e47b2e3375fd1cb85bb0869c8bb17a2330d0
Krishanu Maity <krishanumaity16@gmail.com>
2025-01-26 03:36:52 +0530

    added login page

---
+++ lib/components/TextField.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Textfield extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+  final String labelText;
+
+  const Textfield({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+    required this.labelText,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextFormField(
+                controller: controller,
+                obscureText: obscureText,
+                decoration: InputDecoration(
+                  labelText: labelText,
+                  labelStyle: TextStyle(color: Colors.black),
+                  enabledBorder: OutlineInputBorder(borderRadius:
+                  BorderRadius.circular(22),
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    fillColor: Colors.grey.shade100,
+                    filled: true,
+                    hintText: hintText,
+                ),
+              ),
+            );
+  }
+}
